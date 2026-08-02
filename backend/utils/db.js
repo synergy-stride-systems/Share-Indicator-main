ca62c924948c99d8ea897a631798d1770b75ea43
shreya-gangatirkar <gangatirkarshreya@gmail.com>
2026-08-03 00:13:51 +0530

    db cahnges

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,28 +1,43 @@
 import { DataSource } from "typeorm";
 import { config } from "dotenv";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { User } from "../models/user.model.js";
 import { Strategy } from "../models/stratergy.model.js";
-console.log("DB CONFIG:", {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL,
-});
-config(); 
 
-const sslOptions = process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : undefined;
+config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ssl =
+  process.env.DB_SSL === "true"
+    ? {
+        ca: fs.readFileSync(
+          path.join(__dirname, "../certs/DigiCertGlobalRootG2.crt.pem")
+        ),
+      }
+    : undefined;
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   synchronize: process.env.DB_SYNC === "true",
-  ssl: sslOptions,
-  entities: [User, Strategy], 
+
+  ssl,
+
+  entities: [User, Strategy],
   migrations: [],
   subscribers: [],
+
+  extra: {
+    connectTimeout: 30000,
+  },
 });
